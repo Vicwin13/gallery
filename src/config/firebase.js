b2e61026c833f67a9aee9e4f6c417a0c4c534e19
Winner <vnwimo13@gmail.com>
2023-09-18 19:03:32 +0100

    first commit

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6_VQyULdwKdovtGzq0Lw2w0X87haY8pc",
+  authDomain: "image-gallery-a5c13.firebaseapp.com",
+  projectId: "image-gallery-a5c13",
+  storageBucket: "image-gallery-a5c13.appspot.com",
+  messagingSenderId: "681738796158",
+  appId: "1:681738796158:web:7da085a3c7098bb632ac2f",
+  measurementId: "G-5MVQ0GVF0Z",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
