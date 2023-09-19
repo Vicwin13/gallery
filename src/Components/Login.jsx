be6cfad877952eebb2dd0bc375d4cf094d54928d
Winner <vnwimo13@gmail.com>
2023-09-19 05:52:03 +0100

    firebase auth

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,39 +1,52 @@
+import { useState } from "react";
+import { auth } from "../config/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.PreventDefault();
+  };
+
+  const signIn = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <div>
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
-          <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
-            <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
-              Sign Up
-            </h2>
-            <div className="relative mb-4">
-              <label className="leading-7 text-sm text-gray-600">
-                Username
-              </label>
-              <input
-                type="text"
-                id="username"
-                placeholder="Username"
-                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <div className="relative mb-4">
-              <label className="leading-7 text-sm text-gray-600">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                placeholder="password"
-                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-              Button
+    <div className="flex justify-center  items-center min-h-screen">
+      <section className="border-2 rounded-xl border-green-400 w-[24rem]  p-8  ">
+        <h2 className="text-2xl text-center font-bold ">Sign In</h2>
+        <form action="" onClick={handleSubmit}>
+          <div className=" flex flex-col py-4">
+            <label htmlFor="">Username</label>
+            <input
+              className="border"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className=" flex flex-col py-4">
+            <label htmlFor="">Password</label>
+            <input
+              className="border"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <button
+              onClick={signIn}
+              className="bg-green-400 font-semibold h-8 w-full">
+              Submit
             </button>
           </div>
-        </div>
+        </form>
       </section>
     </div>
   );
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB6_VQyULdwKdovtGzq0Lw2w0X87haY8pc",
@@ -12,3 +13,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+//for Authentication
+export const auth = getAuth(app);
